959bcab3268bb20688300f8bea450f851439eb29
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-19 11:37:49 -0300

    start_fork

---
+++ fork/fork.c
@@ -0,0 +1,22 @@
+//The hangman game is one of the most famous games played on computers. 
+//The Rules of the game are as follows:
+
+    //There is given a word with omitted characters and you need to guess the characters to win the game.
+    //Only 3 chances are available and if you win the Man survives or Man gets hanged.
+    //So, It is the game can be easily designed in C language with the basic knowledge of if-else statements,
+    //loops, and some other basic statements. The code of the game is easy, short, and user-friendly.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    printf("Welcome to the Hangman Game\n");
+
+    char given_word[] = "FILOSOFIA";
+    printf("This is a 9 characters word, in Portuguese.\n");
+    char complete[10] = {'#', '#', '#', '#', '#', '#', '#', '#', '#'};
+
+    
+
+
+}
